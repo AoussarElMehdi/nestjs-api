43c2c8d8763a4bea8877579c29f9c00a9f313819
localhost <elmehdi.asr@gmail.com>
2022-03-04 22:07:06 +0100

    add bookmark service & controller

--- src/bookmark/bookmark.module.ts
+++ src/bookmark/bookmark.module.ts
@@ -1,15 +1,10 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
-import { BookMark, BookMarkSchema, User, UserSchema } from 'src/schemas';
+import { BookmarkService } from './bookmark.service';
+import { BookmarkController } from './bookmark.controller';
 
 @Module({
-    imports: [
-        MongooseModule.forFeature(
-            [
-              { name: User.name, schema: UserSchema },
-              { name: BookMark.name, schema: BookMarkSchema }
-            ]
-          )
-    ]
+    imports: [],
+    providers: [BookmarkService],
+    controllers: [BookmarkController]
 })
 export class BookmarkModule {}
